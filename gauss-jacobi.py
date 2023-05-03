341d9dc93ace858eadb456ac33fafc6d1b578798
Leandro Souza <48530574+Lsouz44@users.noreply.github.com>
2023-05-03 15:27:49 -0300

    Update gauss-jacobi.py

--- gauss-jacobi.py
+++ gauss-jacobi.py
@@ -1,4 +1,4 @@
-def gauss_jacobi(A, b, x0, tol=1e-6, max_iter=5000):
+def gauss_jacobi(A, b, x0, tol=1e-6, max_iter=1000):
     n = len(A)
     x = x0.copy()
     for k in range(max_iter):
@@ -10,8 +10,8 @@ def gauss_jacobi(A, b, x0, tol=1e-6, max_iter=5000):
             return x
     raise Exception(f"O método de Gauss-Jacobi não convergiu após {max_iter} iterações. Última solução encontrada: {x}")
 
-A = [[1, 10, 3], [7, 5, 8], [2, 3, 4]]
-b = [5, 4, 10]
+A = [[2, -1, 0], [-1, 2, -1], [0, -1, 2]]
+b = [1, 3, -2]
 x0 = [0, 0, 0]
 x = gauss_jacobi(A, b, x0)
 print(x)
